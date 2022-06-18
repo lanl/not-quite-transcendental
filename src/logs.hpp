2f34791590480bbe0942b4bbf4c62dc45ebb3077
Jonah Miller <jonahm@lanl.gov>
2022-06-18 17:20:24 -0600

    all set up

---
+++ src/logs.hpp
@@ -0,0 +1,110 @@
+//======================================================================
+// © 2022. Triad National Security, LLC. All rights reserved.  This
+// program was produced under U.S. Government contract 89233218CNA000001
+// for Los Alamos National Laboratory (LANL), which is operated by Triad
+// National Security, LLC for the U.S.  Department of Energy/National
+// Nuclear Security Administration. All rights in the program are
+// reserved by Triad National Security, LLC, and the U.S. Department of
+// Energy/National Nuclear Security Administration. The Government is
+// granted for itself and others acting on its behalf a nonexclusive,
+// paid-up, irrevocable worldwide license in this material to reproduce,
+// prepare derivative works, distribute copies to the public, perform
+// publicly and display publicly, and to permit others to do so.
+//======================================================================
+
+// Code taken from singularity-eos
+// https://github.com/lanl/singularity-eos
+
+#pragma once
+
+#include <cassert>
+#include <cmath>
+
+#include "Kokkos_Core.hpp"
+
+namespace FastMath {
+
+KOKKOS_FORCEINLINE_FUNCTION
+auto as_int(double f) {
+  return *reinterpret_cast<long long int*>(&f);
+}
+KOKKOS_FORCEINLINE_FUNCTION
+auto as_double(long long int i) {
+  return *reinterpret_cast<double*>(&i);
+}
+
+// Reference implementations, however the integer cast implementation
+// below is probably faster.
+/*
+KOKKOS_FORCEINLINE_FUNCTION
+double lg(const double x) {
+  int n;
+  assert(x > 0 && "log divergent for x <= 0");
+  const double y = frexp(x, &n);
+  return 2 * (y - 1) + n;
+}
+
+
+KOKKOS_FORCEINLINE_FUNCTION
+double pow2(const double x) {
+  const int flr = std::floor(x);
+  const double remainder = x - flr;
+  const double mantissa = 0.5 * (remainder + 1);
+  const double exponent = flr + 1;
+  return ldexp(mantissa, exponent);
+}
+*/
+
+KOKKOS_FORCEINLINE_FUNCTION
+double lg(const double x) {
+  // Magic numbers constexpr because C++ doesn't constexpr reinterpret casts
+  // these are floating point numbers as reinterpreted as integers.
+  // as_int(1.0)
+  constexpr long long int one_as_int = 4607182418800017408;
+  // 1./static_cast<double>(as_int(2.0) - as_int(1.0))
+  constexpr double scale_down = 2.22044604925031e-16;
+  return static_cast<double>(as_int(x) - one_as_int) * scale_down;
+}
+
+KOKKOS_FORCEINLINE_FUNCTION
+double pow2(const double x) {
+  // Magic numbers constexpr because C++ doesn't constexpr reinterpret casts
+  // these are floating point numbers as reinterpreted as integers.
+  // as_int(1.0)
+  constexpr long long int one_as_int = 4607182418800017408;
+  // as_int(2.0) - as_int(1.0)
+  constexpr double scale_up = 4503599627370496;
+  return as_double(static_cast<long long int>(x*scale_up) + one_as_int);
+}
+
+KOKKOS_FORCEINLINE_FUNCTION
+double ln(const double x) {
+  constexpr double ILOG2E = 0.6931471805599453;
+  return ILOG2E * lg(x);
+}
+
+KOKKOS_FORCEINLINE_FUNCTION
+double exp(const double x) {
+  constexpr double LOG2E = 1.4426950408889634;
+  return pow2(LOG2E * x);
+}
+
+KOKKOS_FORCEINLINE_FUNCTION
+double log10(const double x) {
+  constexpr double LOG2OLOG10 = 0.301029995663981195;
+  return LOG2OLOG10 * lg(x);
+}
+
+KOKKOS_FORCEINLINE_FUNCTION
+double pow10(const double x) {
+  constexpr double LOG10OLOG2 = 3.321928094887362626;
+  return pow2(LOG10OLOG2 * x);
+}
+
+KOKKOS_FORCEINLINE_FUNCTION
+double tanh(const double x) {
+  const double expx = exp(2 * x);
+  return (expx - 1) / (expx + 1);
+}
+
+} // namespace FastMath
---
+++ src/timings.cpp
@@ -0,0 +1,163 @@
+//======================================================================
+// © 2022. Triad National Security, LLC. All rights reserved.  This
+// program was produced under U.S. Government contract 89233218CNA000001
+// for Los Alamos National Laboratory (LANL), which is operated by Triad
+// National Security, LLC for the U.S.  Department of Energy/National
+// Nuclear Security Administration. All rights in the program are
+// reserved by Triad National Security, LLC, and the U.S. Department of
+// Energy/National Nuclear Security Administration. The Government is
+// granted for itself and others acting on its behalf a nonexclusive,
+// paid-up, irrevocable worldwide license in this material to reproduce,
+// prepare derivative works, distribute copies to the public, perform
+// publicly and display publicly, and to permit others to do so.
+//======================================================================
+
+// C headers
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+
+// C++ headers
+#include <chrono>
+#include <iostream>
+#include <limits>
+
+#include "Kokkos_Core.hpp"
+
+// Local headers
+#include "logs.hpp"
+
+using duration_t = std::chrono::nanoseconds;
+
+constexpr int NTRIALS = 10;
+template <typename Operation, typename Duration = duration_t>
+auto TimeOperation(const Operation &op) {
+  Kokkos::fence();
+  auto start = std::chrono::high_resolution_clock::now();
+  //for (int t = 0; t < NTRIALS; ++t) {
+    op();
+    op();
+    op();
+    op();
+    op();
+    op();
+    op();
+    op();
+    op();
+    op();
+  //  }
+  Kokkos::fence();
+  auto stop = std::chrono::high_resolution_clock::now();
+  return std::chrono::duration_cast<Duration>(stop - start);
+}
+
+template <typename Kernel, typename Duration = duration_t>
+auto TimeKernel(const char *name, const int ntimes, const Kernel &k) {
+  return TimeOperation([=]() { Kokkos::parallel_for(name, ntimes, k); });
+}
+
+KOKKOS_INLINE_FUNCTION
+double get_dx(const double min, const double max, const int N) {
+  return (max - min) / (N - 1);
+}
+
+KOKKOS_INLINE_FUNCTION
+double get_diff(const double a, const double b) {
+  return 2. * std::abs(a - b) /
+         (std::abs(a) + std::abs(b) + std::numeric_limits<double>::epsilon());
+}
+
+int main(int argc, char *argv[]) {
+  Kokkos::initialize(argc, argv);
+  {
+    if (argc < 2) {
+      std::cerr << "Usage: " << argv[0] << " npoints" << std::endl;
+      std::exit(1);
+    }
+
+    const int NPOINTS = std::atoi(argv[1]);
+    double min, max, dx;
+    Kokkos::View<double *> lx("lx", NPOINTS);
+    Kokkos::View<double *> ex("ex", NPOINTS);
+    Kokkos::View<double *> fastex("fastex", NPOINTS);
+    Kokkos::View<double *> scratch("scratch", NPOINTS);
+
+    // exp10
+    min = -10;
+    max = 10;
+    dx = get_dx(min, max, NPOINTS);
+    Kokkos::parallel_for(
+        "setup", NPOINTS, KOKKOS_LAMBDA(const int i) { lx[i] = min + dx * i; });
+    auto dt_std_exp10 = TimeKernel(
+        "std::exp10", NPOINTS, KOKKOS_LAMBDA(const int i) {
+	  ex[i] = std::pow(10, lx[i]);
+        });
+    auto dt_fast_exp10 = TimeKernel(
+        "fast::exp10", NPOINTS, KOKKOS_LAMBDA(const int i) {
+	  fastex[i] = FastMath::pow10(lx[i]);
+        });
+
+    // log10
+    auto dt_std_log10 = TimeKernel(
+        "std::log10", NPOINTS, KOKKOS_LAMBDA(const int i) {
+		  scratch[i] = std::log10(ex[i]);
+        });
+    double std_diff = 0;
+    Kokkos::parallel_reduce(
+        "check diff", NPOINTS,
+        KOKKOS_LAMBDA(const int i, double &d) { d += get_diff(scratch[i], lx[i]); },
+        std_diff);
+
+    auto dt_fast_log10 = TimeKernel(
+        "fast::log10", NPOINTS, KOKKOS_LAMBDA(const int i) {
+	  scratch[i] = FastMath::log10(fastex[i]);
+        });
+    double fast_diff = 0;
+    Kokkos::parallel_reduce(
+        "check diff", NPOINTS,
+        KOKKOS_LAMBDA(const int i, double &d) { d += get_diff(scratch[i], lx[i]); },
+        fast_diff);
+
+    printf("Invertibility of log10 (smaller is better):\n"
+           "\tstdlib: %.14e\n"
+           "\tfast:   %.14e\n",
+           std_diff / static_cast<double>(NPOINTS),
+           fast_diff / static_cast<double>(NPOINTS));
+
+    // tanh
+    auto dt_std_tanh = TimeKernel(
+        "std::tanh", NPOINTS, KOKKOS_LAMBDA(const int i) {
+	  scratch[i] = std::tanh(lx[i]);
+        });
+    auto dt_fast_tanh = TimeKernel(
+        "fast::tanh", NPOINTS, KOKKOS_LAMBDA(const int i) {
+	  scratch[i] = FastMath::tanh(lx[i]);
+        });
+
+    // Report
+    printf("Timings for pow10:\n"
+           "\tstdlib: %.14e (ns/point)\n"
+           "\tfast:   %.14e (ns/point)\n"
+           "\tratio:  %.14e\n"
+           "Timings for log10:\n"
+           "\tstdlib: %.14e (ns/point)\n"
+           "\tfast:   %.14e (ns/point)\n"
+           "\tratio:  %.14e\n"
+           "Timings for tanh:\n"
+           "\tstdlib: %.14e (ns/point)\n"
+           "\tfast:   %.14e (ns/point)\n"
+           "\tratio:  %.14e\n",
+           dt_std_exp10.count() / static_cast<double>(NPOINTS * NTRIALS),
+           dt_fast_exp10.count() / static_cast<double>(NPOINTS * NTRIALS),
+           dt_std_exp10.count() / static_cast<double>(dt_fast_exp10.count()),
+           dt_std_log10.count() / static_cast<double>(NPOINTS * NTRIALS),
+           dt_fast_log10.count() / static_cast<double>(NPOINTS * NTRIALS),
+           dt_std_log10.count() / static_cast<double>(dt_fast_log10.count()),
+           dt_std_tanh.count() / static_cast<double>(NPOINTS * NTRIALS),
+           dt_fast_tanh.count() / static_cast<double>(NPOINTS * NTRIALS),
+           dt_std_tanh.count() / static_cast<double>(dt_fast_tanh.count()));
+  }
+  Kokkos::finalize();
+
+  return 0;
+}
